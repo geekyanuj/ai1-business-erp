aa3cef0761aa314af981623973a497b286cb3843
Anuj Kumar <anujv8692@gmail.com>
2026-09-02 10:30:53 +0530

    Changes the footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="mt-auto bg-light text-end p-2">
-    &copy; {{ date('Y') }} AI1 Business ERP. All rights reserved.
+<footer class="mt-auto bg-light text-end p-2 fs-6">
+    &copy; {{ date('Y') }} AI1 Business ERP. All rights reserved. — Powered by Inertia Automation & Tech
 </footer>
